417ed8a71789bcdd4ddcb5f7a025e8528379cae6
adeosunsam <adeosunsamuel30@gmail.com>
2021-06-27 15:13:12 +0100

    updated app with Console Table

--- TODOAPP/TodoApp.cs
+++ TODOAPP/TodoApp.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TODOAPP
 {
     public class TodoApp
     {
-        List<TodoItem> StoringTodoItem = new List<TodoItem>();
+        List<TodoItem> StoringTodoItem = new List<TodoItem> ();
 
         int SerialNumber = 1;
         public TodoItem AddItem(string TodoItem)
@@ -20,9 +21,14 @@ namespace TODOAPP
         
         public void PrintItem()
         {
+            TabularData.PrintLines();
+            TabularData.PrintHeadings("S/N", "TODO ITEM", "STATUS");
+            TabularData.PrintLines();
+
             foreach (var item in StoringTodoItem)
             {
-                Console.WriteLine($"{item.SerialNumber} {item.Item} {(item.IsDone ? "Done" : "Undone")}");
+                TabularData.PrintHeadings(item.SerialNumber.ToString(), item.Item, (item.IsDone ? "Done" : "Undone"));
+                TabularData.PrintLines();
             }
         }
         public void MarkDone(int Num)
@@ -39,4 +45,31 @@ namespace TODOAPP
             Console.WriteLine (StoringTodoItem.Remove(foundItem));
         }
     }
+    
+    public static class TabularData
+    {
+        private const int TableWidth = 80;
+
+        public static void PrintLines()
+        {
+            Console.WriteLine(new string('-', TableWidth));
+        }
+
+        public static void PrintHeadings(params string[] col)
+        {
+            int Width = (TableWidth - col.Length) / col.Length;
+            const string seed = "|";
+            string row = col.Aggregate(seed, (seperator, colText) => seperator + GetCenterAllignedText(colText, Width) + seed);
+            Console.WriteLine(row);
+
+        }
+
+        private static string GetCenterAllignedText(string colText, int width)
+        {
+            colText = colText.Length > width ? colText.Substring(0, width - 3) + "..." : colText;
+            return string.IsNullOrEmpty(colText) ? new string(' ', width)
+                : colText.PadRight(width - ((width - colText.Length) / 2)).PadLeft(width); 
+        }
+    }
+
 }
